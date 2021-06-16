57dcd1a4f1f21931320f18583641c20f8a0ebedd
J-roma <roma.jhosep@gmail.com>
2021-06-15 22:57:37 -0500

    Buscador

--- src/componentes/Search.jsx
+++ src/componentes/Search.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import styled from 'styled-components'
 import { useDispatch, useSelector } from 'react-redux';
 import StandbyState from './search/StandbyState';
 
-const Container = styled.div`
-
-`
-
 
 const Search = () => {
-
+    const {id} = useParams();
+    
 
     return (
         <div>
